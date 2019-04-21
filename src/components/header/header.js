b1d2f546403bad6114d5ce5928230977d34ea4cf
Varinder Singh <Varinder.Singh@timesinternet.in>
2019-04-21 17:05:06 +0530

    message

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import styles from '../header/header.css';
+import s from '../header/header.css';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+
 class Header extends Component {
     render() {
         return (
             <header>
                 {/* <div className="bold_txt">Varinder</div>
                 <div className="lightbold_txt">FREELANCE  DEVELOPER</div> */}
-<ul><li>Home</li><li>Contact</li></ul>
+<ul className={s.header}><li><a href="/">Home</a></li><li><a href="/contact">Contact</a></li></ul>
             </header>
         );
     }
 }
-
-export default Header;
+export default withStyles(s)(Header);
